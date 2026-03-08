b85a3b07fe5dc8ca6c2565c8759e5301485b573e
unknown <julian.kruk@stud.vilniustech.lt>
2026-03-08 14:47:18 +0200

    feat: add Play, Pause, Stop controls

--- src/js/control.js
+++ src/js/control.js
@@ -2,12 +2,12 @@
   'use strict';
 
   var videoEl = document.getElementById('hlsVideo');
-
-  var currentStream = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+  var hls = null;
 
   function loadStream(url) {
+    if (hls) { hls.destroy(); }
     if (Hls.isSupported()) {
-      var hls = new Hls();
+      hls = new Hls();
       hls.loadSource(url);
       hls.attachMedia(videoEl);
       hls.on(Hls.Events.MANIFEST_PARSED, function () {
@@ -19,6 +19,19 @@
     }
   }
 
-  loadStream(currentStream);
+  document.getElementById('btnPlay').addEventListener('click', function () {
+    videoEl.play();
+  });
+
+  document.getElementById('btnPause').addEventListener('click', function () {
+    videoEl.pause();
+  });
+
+  document.getElementById('btnStop').addEventListener('click', function () {
+    videoEl.pause();
+    videoEl.currentTime = 0;
+  });
+
+  loadStream('https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
 
 }());
